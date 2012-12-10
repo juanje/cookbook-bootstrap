c0023f5c50f60f80fbdb9ffc0773dfb5d27bff04
Juanje Ojeda <juanje.ojeda@gmail.com>
2012-12-10 17:54:02 +0000

    Updated the cookbook version to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Juanje Ojeda"
 maintainer_email "juanje.ojeda@gmail.com"
 license          "Apache 2.0"
 description      "Bootstrap a Aentos Rails project"
-version          "0.1.1"
+version          "0.1.2"
 
 %w{ ubuntu debian }.each do |os|
   supports os
