bad94dfb6d1bfbb79c5ce8ba9c2ea36a5d805350
Juanje Ojeda <juanje.ojeda@gmail.com>
2013-02-14 15:31:03 +0000

    Mayor recipe refactor
    * Split functionalities into new recipes
    * Use a simpler custom rvm recipe instead of external a cookbook
    * Separate the databases into optional recipes
    * Create a optional recipe for testing stuff (firefox, xvnc DISPLAY=:0, etc)
    * Remove old external dependencies

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,27 +13,55 @@ else
   default['bootstrap']['shell'] = '/bin/bash'
 end
 
-default['bootstrap']['postgresql_password'] = 'aentos'
+# Set some handy vars
+default['user_name'] = node['bootstrap']['user']
+default['user_home'] = "/home/#{node['user_name']}"
+default['user_env'] = {
+  'USER' => node['user_name'],
+  'HOME' => node['user_home']
+}
 
+# include these recipes?
+default['bootstrap']['postgresql'] = true
+default['bootstrap']['mongodb'] = true
+default['bootstrap']['testing'] = true
+default['bootstrap']['java'] = true
+
+# Postgresql
+default['bootstrap']['postgresql_password'] = 'aentos'
 include_attribute 'postgresql'
 node.set['postgresql']['password']['postgres'] = node['bootstrap']['postgresql_password']
 
 # Dependencies
-default['bootstrap']['packages'] = %w{icewm vnc4server firefox vim screen tmux git bash-completion openjdk-7-jre-headless}
+default['bootstrap']['packages']['base'] = %w{
+  vim screen tmux git bash-completion
+}
+default['bootstrap']['packages']['testing'] = %w{
+  icewm vnc4server firefox
+}
+default['bootstrap']['packages']['java'] = %w{
+  openjdk-7-jre-headless
+}
+default['bootstrap']['packages']['extra'] = []
 
 # System scripts and settings
-default['bootstrap']['init_scripts'] = %w{icewm xvnc}
+
+default['bootstrap']['init_scripts'] = %w{
+  icewm xvnc
+}
 
 # RVM and gem settings
 default['bootstrap']['rvm']['installer_url'] = "https://get.rvm.io"
 default['bootstrap']['rvm']['branch']  = "stable"
-default['bootstrap']['rvm']['ruby'] = 'ruby-1.9.3'
+default['bootstrap']['rvm']['ruby'] = '1.9.3'
 default['bootstrap']['rvm']['packages'] = %w{
   build-essential openssl libreadline6 libreadline6-dev curl git-core
   zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3
   libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool
   bison ssl-cert subversion pkg-config
 }
-
+default['bootstrap']['rvm']['clean_sources'] = true
 default['bootstrap']['gem_options'] = 'gem: --no-rdoc --no-ri -V'
+
+# Shell stuff
 default['bootstrap']['ps1'] = %q{PS1='\[\e[36m\]\u@\h \[\e[32m\][$rvm_env_string] \[\e[35m\]($(git symbolic-ref HEAD 2> /dev/null | xargs -r basename))\[\e[36m\] \w\$\[\e[m\] '}
--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,35 +1,38 @@
-# Some handy vars
-user_home = "/home/#{node['bootstrap']['user']}"
-rvm_src_path = "#{user_home}/.rvm/src"
-user_env = {
-  'USER' => node['bootstrap']['user'],
-  'HOME' => user_home
-}
-
-
 node['bootstrap']['rvm']['packages'].each do |pkg|
   package pkg
 end
 
 execute "Install rvm" do
   command "curl -L #{node['bootstrap']['rvm']['installer_url']} | bash -s #{node['bootstrap']['rvm']['branch']}"
-  user node['bootstrap']['user']
-  cwd  user_home
-  environment user_env
-  creates "#{user_home}/.rvm/scripts/rvm"
+  user node['user_name']
+  cwd  node['user_home']
+  environment node['user_env']
+  creates "#{node['user_home']}/.rvm/scripts/rvm"
 end
 
 bash "Install ruby #{node['bootstrap']['rvm']['ruby']}" do
   code "source $HOME/.rvm/scripts/rvm ; rvm install #{node['bootstrap']['rvm']['ruby']}"
-  user node['bootstrap']['user']
-  cwd  user_home
-  environment user_env
-  creates "#{user_home}/.rvm/rubies/#{node['rvm']['ruby']}/config"
+  user node['user_name']
+  cwd  node['user_home']
+  environment node['user_env']
+  creates "#{node['user_home']}/.rvm/rubies/#{node['bootstrap']['rvm']['ruby']}/config"
   subscribes :run, resources(:execute => "Install rvm")
 end
 
-execute "remove rvm sources" do
-  command "rm -fr #{rvm_src_path}/*"
-  only_if { ::File.exists? "#{rvm_src_path}/rvm" }
-  subscribes :run, resources(:bash => "Install ruby #{node['bootstrap']['rvm']['ruby']}")
+if node['bootstrap']['rvm']['clean_sources']
+  rvm_src_path = "#{node['user_home']}/.rvm/src"
+  execute "remove rvm sources" do
+    command "rm -fr #{rvm_src_path}/*"
+    only_if { ::File.exists? "#{rvm_src_path}/rvm" }
+    subscribes :run, resources(:bash => "Install ruby #{node['bootstrap']['rvm']['ruby']}")
+  end
+end
+
+# Create a .gemrc for the aentos user
+file "#{node['user_home']}/.gemrc" do
+  content node['bootstrap']['gem_options']
+  owner  node['user_name']
+  group  node['user_name']
+  mode   0644
+  action :create_if_missing
 end
